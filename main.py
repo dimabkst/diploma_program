f0efbdb3d96d39a26b84e8530ceb4a7058ba556c
Дмитро Бобик <dimabkst@gmail.com>
2023-04-25 15:57:48 +0300

    always use exp() for exponent

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from view import View
 from traceback import print_exc
+from parsings import parse_function
 
 if __name__ == "__main__":
     try:
+        # USE exp not e in view
         View("data.json")
 
     except Exception as e:
