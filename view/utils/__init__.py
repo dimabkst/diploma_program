41272507757f5e4d10d849195c58464f34ba2b69
Дмитро Бобик <dimabkst@gmail.com>
2023-05-15 19:26:52 +0300

    initial and desired conditions cas

--- view/utils/__init__.py
+++ view/utils/__init__.py
@@ -1,4 +1,4 @@
 from .logger import TextHandler
 from .align_rows_cols import align_rows_cols
 from .create_plot import create_plot
-from .change_and_show import change_and_show_1dim, change_and_show_2dim
+from .change_and_show import change_and_show_1dim, change_and_show_2dim, change_and_show_2dim_desired
