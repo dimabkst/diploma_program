a4764c3b16a4c9eda9dbdfc31aaf11ea4eb7802f
Дмитро Бобик <dimabkst@gmail.com>
2023-05-16 15:56:54 +0300

    view imports

--- view/utils/frames.py
+++ view/utils/frames.py
@@ -1,18 +1,19 @@
-from tkinter import ttk, N, W, E, S
+from tkinter import N, W, E, S
+from tkinter.ttk import Frame
 
 
 def create_frame_label_entrie_frames(root, column: int, row: int, isRow: bool, style: str = "WhiteBg.TFrame",
                                      label_frame_style="WhiteBg.TFrame", entrie_frame_style="WhiteBg.TFrame",  padding: str = "3 3 12 12"):
     try:
-        frame = ttk.Frame(root, padding=padding, style=style)
+        frame = Frame(root, padding=padding, style=style)
         frame.grid(column=column, row=row, sticky=(N, W, E, S))
 
-        label_frame = ttk.Frame(frame, padding=padding,
-                                style=label_frame_style)
+        label_frame = Frame(frame, padding=padding,
+                            style=label_frame_style)
         label_frame.grid(column=0, row=0, sticky=(N, W, E, S))
 
-        entries_frame = ttk.Frame(frame, padding=padding,
-                                  style=entrie_frame_style)
+        entries_frame = Frame(frame, padding=padding,
+                              style=entrie_frame_style)
         entries_frame.grid(column=1*isRow, row=1 * (not isRow),
                            sticky=(N, W, E, S))
 
@@ -26,13 +27,13 @@ def create_label_entrie_frames(root,
                                entry_frame_column, entry_frame_row,
                                label_frame_style="WhiteBg.TFrame", entrie_frame_style="WhiteBg.TFrame",  padding: str = "3 3 12 12"):
     try:
-        label_frame = ttk.Frame(root, padding=padding,
-                                style=label_frame_style)
+        label_frame = Frame(root, padding=padding,
+                            style=label_frame_style)
         label_frame.grid(column=label_frame_column, row=label_frame_row,
                          sticky=(N, W, E, S))
 
-        entries_frame = ttk.Frame(root, padding=padding,
-                                  style=entrie_frame_style)
+        entries_frame = Frame(root, padding=padding,
+                              style=entrie_frame_style)
         entries_frame.grid(column=entry_frame_column, row=entry_frame_row,
                            sticky=(N, W, E, S))
 
